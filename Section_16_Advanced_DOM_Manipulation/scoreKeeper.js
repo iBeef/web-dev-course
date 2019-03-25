da3de311e69e448ec5d8e4a368653e097d813913
Lewis Holmes <ibeef1990@gmail.com>
2019-03-25 13:06:18 +0000

    Completed Score Keeper lecture 1

---
+++ Section_16_Advanced_DOM_Manipulation/scoreKeeper.js
@@ -0,0 +1,34 @@
+var p1Button = document.querySelector('#p1Button');
+var p2Button = document.getElementById('p2Button');
+var p1Display = document.querySelector('#p1Display')
+var p2Display = document.querySelector('#p2Display')
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
+
+p1Button.addEventListener("click", function() {
+    if (!gameOver) {
+        p1Score++;
+        if (p1Score === winningScore) {
+            gameOver = true;
+        }
+        p1Display.textContent = p1Score;
+        console.log(p1Score);
+    }
+});
+
+var resetButton = document.querySelector("#resetButton");
+
+p2Button.addEventListener("click", function() {
+    if (!gameOver) {
+        p2Score++;
+        if (p2Score === winningScore) {
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;
+        console.log(p2Score);
+    }
+});
+
+var resetButton = document.querySelector("#resetButton");
