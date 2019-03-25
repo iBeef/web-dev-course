8ace6b96980af22cc529c801dffc551b5f6c38c6
Lewis Holmes <ibeef1990@gmail.com>
2019-03-25 22:03:20 +0000

    Finished score keeper lecture pt. 2

--- Section_16_Advanced_DOM_Manipulation/scoreKeeper.js
+++ Section_16_Advanced_DOM_Manipulation/scoreKeeper.js
@@ -1,7 +1,10 @@
 var p1Button = document.querySelector('#p1Button');
 var p2Button = document.getElementById('p2Button');
+var resetButton = document.getElementById('reset');
 var p1Display = document.querySelector('#p1Display')
 var p2Display = document.querySelector('#p2Display')
+var numInput = document.querySelector("input")
+var winningScoreDisplay = document.querySelector('p span');
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -11,6 +14,7 @@ p1Button.addEventListener("click", function() {
     if (!gameOver) {
         p1Score++;
         if (p1Score === winningScore) {
+            p1Display.classList.add("winner")
             gameOver = true;
         }
         p1Display.textContent = p1Score;
@@ -18,12 +22,11 @@ p1Button.addEventListener("click", function() {
     }
 });
 
-var resetButton = document.querySelector("#resetButton");
-
 p2Button.addEventListener("click", function() {
     if (!gameOver) {
         p2Score++;
         if (p2Score === winningScore) {
+            p2Display.classList.add("winner")
             gameOver = true;
         }
         p2Display.textContent = p2Score;
@@ -31,4 +34,22 @@ p2Button.addEventListener("click", function() {
     }
 });
 
-var resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener("click", function() {
+    reset();
+});
+
+function reset() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('winner')
+    p2Display.classList.remove('winner')
+    gameOver = false;
+};
+
+numInput.addEventListener("change", function() {
+    winningScoreDisplay.textContent = this.value;
+    winningScore = Number(this.value);
+    reset();
+});
