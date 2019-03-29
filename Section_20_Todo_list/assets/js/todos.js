f5a43630ccd43e4d32e7f009bdf99aa50f68a3a1
Lewis Holmes <ibeef1990@gmail.com>
2019-03-29 07:51:13 +0000

    Finished deleting todos lecture.

--- Section_20_Todo_list/assets/js/todos.js
+++ Section_20_Todo_list/assets/js/todos.js
@@ -1,1 +1,25 @@
-alert("Working!!!");
+// Check off specific todos by clicking.
+$('li').click(function() {
+    $(this).toggleClass('completed');
+});
+
+$('span').click(function(event) {
+    console.log('clicked span');
+    event.stopPropagation();
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+
+})
+
+$('ul').click(function() {
+    console.log('clicked ul');
+});
+
+$('#container').click(function() {
+    console.log('clicked span');
+});
+
+$('body').click(function() {
+    console.log('clicked body');
+});
