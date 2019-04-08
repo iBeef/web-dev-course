e68ca72a1b367682290bb6a786663cc1cbc642b8
Lewis Holmes <ibeef1990@gmail.com>
2019-04-08 22:03:45 +0100

    Reached lecture on JSON and XML

--- Section_26_Intermediate_Express/post_request_demo/app.js
+++ Section_26_Intermediate_Express/post_request_demo/app.js
@@ -1,18 +1,24 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var friends = ['Tony', 'Lilly', 'Steve', 'Alexios', 'Dan'];
 
 app.get('/', function(req, res) {
     res.render('home');
 });
 
 app.post('/addfriend', function(req, res) {
-    res.send("You have reached the post route!");
+    var newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    res.redirect('/friends');
 });
 
 app.get('/friends', function(req, res) {
-    var friends = ['Tony', 'Lilly', 'Steve', 'Alexios', 'Dan'];
     res.render('friends', {friends: friends});
 });
 
