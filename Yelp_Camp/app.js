6e929541c9ede230f4bdebe8454b4aff6ddc25e4
Lewis Holmes <ibeef1990@gmail.com>
2019-04-11 21:24:43 +0100

    Finsihed show campgrounds lecture 1

--- Yelp_Camp/app.js
+++ Yelp_Camp/app.js
@@ -9,27 +9,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", 'ejs');
 
 // Setup DB
-mongoose.connect("mongodb://localhost:27017/yelp_camp");
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 
 // Schema setup
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-// Campground.create({
-//     name: "Granite Hill",
-//     image: "https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg"
-// }, (err, campground) => {
-//     if(err) {
-//         console.log(err);
-//     } else {
-//         console.log("Newly created campground: ");
-//         console.log(campground);
-//     }
-// });
+Campground.create({
+    name: "Granite Hill",
+    image: "https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg",
+    description: "This is a huge granite hill, no bathrooms. No water. Beautiful Granite!"
+}, (err, campground) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("Newly created campground: ");
+        console.log(campground);
+    }
+});
 
 // GET routes
 app.get('/', (req, res) => {
@@ -49,6 +51,12 @@ app.get('/campgrounds', (req, res) => {
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('new');
+});
+
+app.get("/campgrounds/:id", (req, res) => {
+    //  Find campground with provided id.
+    // Render the show twmplate with that campground
+    res.send("This will be the show page one day");
 });
 
 // POST routes
