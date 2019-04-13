1039c0e3c0dc4bd43aa99dae06d81e510e3bb174
Lewis Holmes <ibeef1990@gmail.com>
2019-04-13 17:39:25 +0100

    Completed Modify and update lecture for RESTful blog app.

--- Section_31_RESTful_Routing/RESTful_Blog_App/app.js
+++ Section_31_RESTful_Routing/RESTful_Blog_App/app.js
@@ -1,12 +1,14 @@
-var bodyParser = require("body-parser"),
-    express    = require('express'),
-    mongoose   = require('mongoose');
-    app        = express();
+var bodyParser     = require("body-parser"),
+    express        = require('express'),
+    methodOverride = require("method-override"),
+    mongoose       = require('mongoose'),
+    app            = express();
 
 // Setup app
 app.set("view engine", 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"))
 
 // Setup DB
 mongoose.connect(
@@ -28,12 +30,64 @@ app.get("/", (req, res) => {
     res.redirect("/blogs");
 });
 
+//  Index
 app.get("/blogs", (req, res) => {
     Blog.find({}, (err, results) => {
         if(err) {
             console.log("OOOOPS! Something went wrong!");
         } else {
             res.render('index', {blogs: results});
+        }
+    });
+});
+
+// New Route
+app.get("/blogs/new", (req, res) => {
+    res.render('new');
+});
+
+// Create Route
+app.post("/blogs", (req, res) => {
+    // Create blog
+    Blog.create(req.body.blog, (err, result) => {
+        if(err) {
+            res.render('new')
+        } else {
+            // Redirect
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// Show Route
+app.get("/blogs/:id", (req, res) => {
+    Blog.findById(req.params.id, (err, result) => {
+        if(err) {
+            res.redirect("/blogs")
+        } else {
+            res.render('show', {blog: result});
+        }
+    });
+});
+
+// Edit Route
+app.get("/blogs/:id/edit", (req, res) => {
+    Blog.findById(req.params.id, (err, result) => {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render('edit', {blog: result});
+        }
+    });
+});
+
+// Update Route
+app.put("/blogs/:id", (req, res) => {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, result) => {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect(`/blogs/${req.params.id}`);
         }
     });
 });
